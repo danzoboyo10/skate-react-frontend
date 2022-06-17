5133287c1c07b5ea3a8fa8075d724c674068a81d
Peter Yoo <trustintofu@gmail.com>
2022-06-16 18:54:06 -0700

    wip: css edits on home

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <header>
-      <Link to="/">Skate React</Link><br></br>
+      <Link to="/"><img src="https://i.imgur.com/kn88Yah.png" /></Link><br></br>
     </header>
   );
 };
