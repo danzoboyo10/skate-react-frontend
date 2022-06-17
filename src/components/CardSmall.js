20b1d241e5b32421d90b17ca097974e6dbf99d37
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-06-17 15:48:35 -0400

    wip:uses optional chaining to fix display of json data

--- src/components/CardSmall.js
+++ src/components/CardSmall.js
@@ -3,19 +3,19 @@ const loaded = () => {
   return props.boards.map((boards) => (
     <div className="cardSmall_card">
       {console.log(boards)}
-      {console.log(boards.board.name)} 
-      <section className="cardSmall_card--name">{boards.board.name}</section>
-      <section className="cardSmall_card--img"><img src={boards.board.bigImg} alt={boards.board.name}/></section>
-      <section className="cardSmall card--price">{boards.board.price}</section>
+      {console.log(boards?.board?.name)} 
+      <section className="cardSmall_card--name">{boards?.board?.name}</section>
+      <section className="cardSmall_card--img"><img src={boards?.board?.bigImg} alt={boards?.board?.name}/></section>
+      <section className="cardSmall card--price">{boards?.board?.price}</section>
     </div>
-  ))
+  ));
 
-}
+};
 const loading = () => {
   return <h1>loading...</h1>
-}
+};
 
-return boards ? loaded() : loading();
+return props.boards ? loaded() : loading(); 
 };
 
 export default CardSmall;
