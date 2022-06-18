ee84914efdec1f0bd5ac5ac9677f9e8307a9d9cb
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-06-18 17:16:16 -0400

    wip: adds partial functionality to create pg

--- src/components/Board/BoardCreate.js
+++ src/components/Board/BoardCreate.js
@@ -3,17 +3,55 @@ import BoardCart from "./BoardCart"
 import BoardDescription from "./BoardDescription"
 import BoardWidth from "./BoardWidth"
 import BoardIframe from "./BoardIframe"
+import { useState } from "react"
 
-const BoardCreate = () => {
+const BoardCreate = (props) => {
+  const [newForm, setNewForm ] = useState({
+    name: "",
+  });
+
+  const handleChange = (event) => {
+    setNewForm({
+      ...newForm,
+      [event.targent.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createBoard(newForm);
+  }
+
+  const loaded = () => {
+    return (
+      <div className="board-create">
+        <BoardTitlePrice />
+        <BoardWidth />
+        <BoardIframe />
+        <BoardCart />
+        <BoardDescription />
+      </div>
+    )
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>
+  }
+  
   return (
-    <div className="board-create">
-      <BoardTitlePrice />
-      <BoardWidth />
-      <BoardIframe />
-      <BoardCart />
-      <BoardDescription />
-    </div>
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={newForm.name}
+          onChange={handleChange}
+          name="name"
+          type="text"
+        />
+        <input type="submit" value="Create Skateboard" />
+      </form>
+      {props.board ? loaded() : loading()};
+    </section>
   )
-}
+};
 
 export default BoardCreate
