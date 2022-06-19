8f94bc7b275c7152793137e29eb3538632145586
Peter Yoo <trustintofu@gmail.com>
2022-06-19 14:09:10 -0700

    wip: refactors css to be more responsive

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -6,6 +6,12 @@ import { useState } from "react"
 const Board = (props) => {
 
   const [ skateBoards, setSkateBoards ] = useState(null); 
+
+  const getBoards = async (board) => {
+    const response = await fetch(props.URL + 'create');
+    const data = await response.json();
+    setSkateBoards(data);
+  }
   
   const createBoards = async (board) => {
     await fetch(props.URL + 'create', {
@@ -15,16 +21,18 @@ const Board = (props) => {
       },
       body: JSON.stringify(board),
     });
+    getBoards();
   }; 
 
   return (
     <div className="board">
-    <div className="board-left-display"> 
-      <BoardDisplay />
-    </div>
-    <div className="board-right-display">
-      <BoardCreate createBoard={createBoards} />
-    </div>
+      <div className="board-left-display"> 
+        <BoardDisplay />
+      </div>
+      <div className="board-right-display">
+        <BoardCreate createBoard={createBoards} />
+      </div>
+      <div className="min-height"></div>
     </div>
   )
 }
