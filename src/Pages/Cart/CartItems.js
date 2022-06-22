ac0cc5c62b0f2f8210822d4aba8faeeb9dbb7c93
Peter Yoo <trustintofu@gmail.com>
2022-06-21 20:45:30 -0700

    wip: displays cart items in cart, adds delete functionality to cart items, adds styling to pages.

--- src/Pages/Cart/CartItems.js
+++ src/Pages/Cart/CartItems.js
@@ -1,10 +1,24 @@
+import { useParams } from "react-router-dom";
+
 const CartItems = (props) => {
+  console.log(props)
+  const { id } = useParams();
+
+  const handleRemoveCartItem = (id) => {
+    props.deleteCartItems(id);
+    props.history.push('/cart');
+  };
 
   const loaded = () => {
-    return props?.cartItems?.map((items) => (
-      <>
-        {items?.board?.name}<br></br>
-      </>
+    return props.cartItems.map((items) => (
+      <div className="cart-items--list">
+        <section className="cart-items--list-name">
+          {items.buildId}<br></br>
+        </section>
+        <section className="cart-items--list-delete">
+          <button onClick={() => handleRemoveCartItem(items._id)}>Delete</button>
+        </section>
+      </div>
     ));
   };
 
@@ -12,7 +26,7 @@ const CartItems = (props) => {
     return <h1>Loading...</h1>
   };
 
-  return props?.cartItems ? loaded() : loading();
+  return props.cartItems ? loaded() : loading();
 }
 
 export default CartItems
